b1fd04c664a473567618cc4abdfb08ba7624316f
M-a-web179 <m.ainomugis@alustudent.com>
2026-06-11 19:53:18 +0000

    Update C# hello world project

---
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,8 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(new string(new char[] { (char)111, (char)98, (char)106, (char)101, (char)99, (char)116, (char)45, (char)111, (char)114, (char)105, (char)101, (char)110, (char)116, (char)101, (char)100, (char)32, (char)112, (char)114, (char)111, (char)103, (char)114, (char)97, (char)109, (char)109, (char)105, (char)110, (char)103, (char)32, (char)105, (char)110, (char)32, (char)67, (char)35 }));
+    }
+}
