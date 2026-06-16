1073305059bdfaf4997cb0fd8408702a37ccd875
M-a-web179 <m.ainomugis@alustudent.com>
2026-06-16 14:11:52 +0000

    test

--- csharp-classes/0-enemy/obj/Debug/net7.0/0-enemy.AssemblyInfo.cs
+++ csharp-classes/0-enemy/obj/Debug/net7.0/0-enemy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0-enemy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24b103190350fb63b88af22638081aab89b9d80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b9c6343439fb6b48b3d2ccf189d54c1dd0a481")]
 [assembly: System.Reflection.AssemblyProductAttribute("0-enemy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0-enemy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
